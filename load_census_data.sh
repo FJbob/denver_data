25ffb9898edbc8b86767854f3c6022022f316020
FJbob <robertmetthe@gmail.com>
2016-05-31 15:17:04 -0500

    Add files via upload

---
+++ load_census_data.sh
@@ -0,0 +1,7 @@
+#curl -XDELETE 'http://localhost:9200/denver_crime/'
+
+#curl -XDELETE 'http://localhost:9200/.kibana/'
+
+#curl -XPUT 'http://localhost:9200/denver_crime' -d @crime_mapping.json --header "Content-Type: application/json"
+
+../logstash-2.2.2/bin/logstash -f census-logstash.js -v
